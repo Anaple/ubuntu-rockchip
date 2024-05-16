d5dbda7581a3dfb4fce7e9ba4ac150ec89c8dd5f
Anaple <67675386+Anaple@users.noreply.github.com>
2024-05-16 20:30:36 +0800

    Update rock-5a.sh

--- config/boards/rock-5a.sh
+++ config/boards/rock-5a.sh
@@ -10,21 +10,5 @@ function config_image_hook__rock-5a() {
     local rootfs="$1"
     local overlay="$2"
 
-    # Install panfork
-    chroot "${rootfs}" add-apt-repository -y ppa:jjriek/panfork-mesa
-    chroot "${rootfs}" apt-get update
-    chroot "${rootfs}" apt-get -y install mali-g610-firmware
-    chroot "${rootfs}" apt-get -y dist-upgrade
-
-    # Fix Bluetooth not working with Radxa RTL8852BE WiFi + BT card
-    cp "${overlay}/usr/lib/systemd/system/radxa-a8-bluetooth.service" "${rootfs}/usr/lib/systemd/system/radxa-a8-bluetooth.service"
-    chroot "${rootfs}" systemctl enable radxa-a8-bluetooth
-
-    # Fix and configure audio device
-    mkdir -p "${rootfs}/usr/lib/scripts"
-    cp "${overlay}/usr/lib/scripts/alsa-audio-config" "${rootfs}/usr/lib/scripts/alsa-audio-config"
-    cp "${overlay}/usr/lib/systemd/system/alsa-audio-config.service" "${rootfs}/usr/lib/systemd/system/alsa-audio-config.service"
-    chroot "${rootfs}" systemctl enable alsa-audio-config
-
     return 0
 }
